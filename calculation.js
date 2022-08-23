efec00a65eb76331a5c56e5c640d2edfb9a31364
RsRuh <rshridoysr@gmail.com>
2022-08-23 12:18:51 +0600

    manager coach cost add

--- calculation.js
+++ calculation.js
@@ -1,5 +1,6 @@
 
-//calculating players cost
+//Calculating Players Cost.....
+
 document.getElementById('playerCostBtn').addEventListener('click', function () {
     const perPlayerCost = document.getElementById('inputOfPerPlayer');
     const playersCostValue = parseFloat(perPlayerCost.value);
@@ -16,3 +17,18 @@ document.getElementById('playerCostBtn').addEventListener('click', function () {
 
 
 
+// Players, Manager And Coach Cost Calculating.....
+
+document.getElementById('totalCalculate').addEventListener('click', function () {
+    const perPlayerCost = document.getElementById('inputOfPerPlayer');
+    const playersCostValue = parseFloat(perPlayerCost.value);
+    if (isNaN(playersCostValue) || playersCostValue < 0) {
+        alert('Please Provide A Valid Number In All Input');
+        return;
+    }
+    const playerList = document.getElementById('player-list').childElementCount;
+    const totalExpense = playersCostValue * playerList;
+    const totalPlayerCostElement = document.getElementById('playerExpenses');
+    totalPlayerCostElement.innerText = totalExpense;
+
+});
